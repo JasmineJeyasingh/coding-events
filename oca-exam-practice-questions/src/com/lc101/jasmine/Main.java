4372454fb56ec3c045916bd1c5a6af495de3b8fe
Jasmine <jasmine.a.jeyasingh@gmail.com>
2022-05-19 19:43:22 -0500

    First commit

---
+++ oca-exam-practice-questions/src/com/lc101/jasmine/Main.java
@@ -0,0 +1,8 @@
+package com.lc101.jasmine;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ oca-exam-practice-questions/src/com/lc101/jasmine/practicetest1/Question1.java
@@ -0,0 +1,8 @@
+package com.lc101.jasmine.practicetest1;
+
+public class Question1 {
+
+        public static void main(String[] args) {
+            System.out.println("Output is: " + 10 != 5);
+        }
+}
